827df897b635505f1ab30763c55bf19849dcd147
25thOliver <os679736@gmail.com>
2026-02-25 09:45:15 +0300

    Embed during scrape

--- app/services/scrape_service.py
+++ app/services/scrape_service.py
@@ -1,6 +1,8 @@
 from app.scraping.destination_scraper import DestinationScraper
 from app.db.session import AsyncSessionLocal
 from app.repositories.destination_repository import DestinationRepository
+from app.services.embedding_service import EmbeddingService
+from app.services.vector_service import VectorService
 
 
 class ScrapeService:
@@ -8,12 +10,29 @@ class ScrapeService:
         scraper = DestinationScraper()
         data = await scraper.scrape_destinations()
 
+        embedding_Service = EmbeddingService()
+        vector_service = VectorService()
+
         count = 0
 
         async with AsyncSessionLocal() as db:
             repo = DestinationRepository(db)
 
             for item in data:
-                await repo.create(**item)
+                db_obj = await repo.create(**item)
+
+                text_for_embeding = (
+                    f"{item['title']} - {item['description']}"
+                )
+
+                vector = await embedding_Service.embed(text_for_embeding)
+
+                vector_service.upsert(
+                    id=db_obj.id,
+                    vector=vector,
+                    payload=item,
+                )
+
                 count += 1
+                
         return count
